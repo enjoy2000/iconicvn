e7c68c1389a25a88c76656f0c59151d314a07e17
enjoy2000 <enjoy3013@gmail.com>
2014-01-07 11:22:38 +0700

    add upload cv attribute

--- app/code/local/Iconic/Job/sql/job_setup/upgrade-0.1.9-0.2.0.php
+++ app/code/local/Iconic/Job/sql/job_setup/upgrade-0.1.9-0.2.0.php
@@ -32,6 +32,19 @@ $installer->addAttribute('customer', 'birth_year', array(
     'visible_on_front' => 1,
         'source' => 'job/entity_birth_year',
 ));
+
+$installer->addAttribute('customer', 'upload_cv', array(
+    'type' => 'varchar',
+    'input' => 'input',
+    'label' => $trans->__('CV'),
+    'global' => 1,
+    'visible' => 1,
+    'required' => 0,
+    'user_defined' => 1,
+    'default' => '0',
+    'visible_on_front' => 1,
+        'source' => 'job/entity_upload_cv',
+));
  
 $installer->run("
  
