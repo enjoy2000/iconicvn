c90265fb379b6a49150f63137f14cdbca00ea463
enjoy2000 <enjoy3013@gmail.com>
2014-01-10 06:56:43 +0700

    fix set url key if exist

--- app/code/local/Iconic/Job/Model/Job.php
+++ app/code/local/Iconic/Job/Model/Job.php
@@ -15,7 +15,8 @@ class Iconic_Job_Model_Job extends Mage_Core_Model_Abstract
             if(!Mage::getModel('job/job')->load($urlKey, 'url_key')->getId()){
                 $this->setUrlKey($urlKey);
             } else {
-                $this->setUrlKeyNotSetted(true);
+                $urlKey .= '-' . $this->getId();
+            	$this->setUrlKey($urlKey);
             }
         }
         parent::_beforeSave();
@@ -23,13 +24,6 @@ class Iconic_Job_Model_Job extends Mage_Core_Model_Abstract
     
     protected function _afterSave()
     {
-        if($this->getUrlKeyNotSetted()){
-            $urlKey = Mage::helper('job')->formatUrlKey($this->getTitle());
-            $urlKey .= '-' . $this->getId();
-            $this->setUrlKey($urlKey);
-            $this->save();
-            $this->setUrlKeyNotSetted(false);
-        }
         parent::_afterSave();
     } 
 
