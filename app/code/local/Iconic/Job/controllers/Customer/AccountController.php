7d9f9497f75441d62d5c45d6d251c4a733db4fdc
enjoy2000 <enjoy3013@gmail.com>
2014-01-20 14:23:17 +0700

    override forgot password post Action

--- app/code/local/Iconic/Job/controllers/Customer/AccountController.php
+++ app/code/local/Iconic/Job/controllers/Customer/AccountController.php
@@ -52,4 +52,48 @@ class Iconic_Job_Customer_AccountController extends Mage_Customer_AccountControl
         $errUrl = $this->_getUrl('*/*/create', array('_secure' => true));
         $this->_redirectError($errUrl);
     }
+
+	
+    /**
+     * Forgot customer password action
+     */
+    public function forgotPasswordPostAction()
+    {
+        $email = (string) $this->getRequest()->getPost('email');
+        if ($email) {
+            if (!Zend_Validate::is($email, 'EmailAddress')) {
+                $this->_getSession()->setForgottenEmail($email);
+                $this->_getSession()->addError($this->__('Invalid email address.'));
+                $this->_redirect('*/*/forgotpassword');
+                return;
+            }
+
+            /** @var $customer Mage_Customer_Model_Customer */
+            $customer = $this->_getModel('customer/customer')
+                ->setWebsiteId(Mage::app()->getStore()->getWebsiteId())
+                ->loadByEmail($email);
+
+            if ($customer->getId()) {
+                try {
+                    $newResetPasswordLinkToken =  $this->_getHelper('customer')->generateResetPasswordLinkToken();
+                    $customer->changeResetPasswordLinkToken($newResetPasswordLinkToken);
+                    $customer->sendPasswordResetConfirmationEmail();
+                } catch (Exception $exception) {
+                    $this->_getSession()->addError($exception->getMessage());
+                    $this->_redirect('*/*/forgotpassword');
+                    return;
+                }
+            }
+            $this->_getSession()
+                ->addSuccess( $this->_getHelper('customer')
+                ->__('Nếu có tài khoản đăng ký với email %s, bạn sẽ nhận được email để thiết lập lại mật khẩu.',
+                    $this->_getHelper('customer')->escapeHtml($email)));
+            $this->_redirect(Mage::helper('job')->getLoginUrl());
+            return;
+        } else {
+            $this->_getSession()->addError($this->__('Please enter your email.'));
+            $this->_redirect('*/*/forgotpassword');
+            return;
+        }
+    }
 }
