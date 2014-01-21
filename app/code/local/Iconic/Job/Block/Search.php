b8a44a4896a699eab72235f59b470138ad1603ed
enjoy2000 <enjoy3013@gmail.com>
2014-01-21 17:05:03 +0700

    update

--- app/code/local/Iconic/Job/Block/Search.php
+++ app/code/local/Iconic/Job/Block/Search.php
@@ -92,7 +92,8 @@ class Iconic_Job_Block_Search extends Mage_Core_Block_Template
 		//get list location and category
 		if (!$this->hasData('locationList')){
 			$location = Mage::getModel('job/location')->getCollection()
-						->addFieldToFilter('name',array('neq'=>'Hồ Chí Minh','neq'=>'Hà Nội'));
+						->addFieldToFilter('name',array('neq'=>'Hồ Chí Minh','neq'=>'Hà Nội'))
+						->setOrder('name','ASC');
 			$listLocation = '<option value="'.Mage::getModel('job/location')->load('Hồ Chí Minh','name').'">Hồ Chí Minh</option><option value="'.Mage::getModel('job/location')->load('Hà Nội','name').'">Hà Nội</option>';
 			if ($this->getLocation()){
 				foreach ($location as $loc){
