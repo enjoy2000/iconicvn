df23f7e8c21e2faf12d997e304e90a5f0db2368f
enjoy2000 <enjoy3013@gmail.com>
2014-02-28 14:38:09 +0700

    git add create cv render for user admin grid

---
+++ app/code/local/Mage/Adminhtml/Block/Customer/Grid/Renderer/Cv.php
@@ -0,0 +1,15 @@
+<?php
+class Mage_Adminhtml_Block_Customer_Grid_Renderer_Cv
+    extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Action
+{
+    public function render(Varien_Object $row)
+    {
+        if($row->getData($this->getColumn()->getIndex())=="0"){
+            return "";
+        }else{
+        	$customer = Mage::getModel('customer/customer')->load($row->getId());
+			$html = '<a target="_blank" href="'.Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB). 'tmp/' . $customer->getEmail() . '.xlsx">Link</a>';
+			return $html;
+        }
+    }
+}
