e6bea1ff18f052af0ee072b2462785615f70dea9
enjoy2000 <enjoy3013@gmail.com>
2014-03-04 09:19:39 +0700

    focus before change

--- js/iconic/user.js
+++ js/iconic/user.js
@@ -8,6 +8,9 @@
 		
 		//style for select placeholder
 		$('select').each(function(){
+			$(this).focus(function(){
+				$(this).css('color', 'black');
+			});
 			$(this).change(function () {
 			    if($(this).val() == "") $(this).css('color', '#A6A6A6');
 			    else $(this).css("color","black");
