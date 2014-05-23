195ea19d05e66e3038253f1b68774e4d1939309a
enjoy2000 <enjoy3013@gmail.com>
2014-05-23 13:36:04 +0700

    automatic set urlkey for location

--- app/code/local/Iconic/Job/Model/Location.php
+++ app/code/local/Iconic/Job/Model/Location.php
@@ -7,4 +7,18 @@ class Iconic_Job_Model_Location extends Mage_Core_Model_Abstract
         parent::_construct();
         $this->_init('job/location');
     }
+	
+	protected function _beforeSave()
+    {
+        if(!$this->getUrlKey()){
+            $urlKey = Mage::helper('job')->formatUrlKey($this->getName());
+            if(!Mage::getModel('job/location')->load($urlKey, 'url_key')->getId()){
+                $this->setUrlKey($urlKey);
+            } else {
+                $urlKey .= '-' . $this->getId();
+            	$this->setUrlKey($urlKey);
+            }
+        }
+        parent::_beforeSave();
+    }
 }
